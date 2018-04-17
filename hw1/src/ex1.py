a611595cd8eb333ae06d40fddb9cdc5904c811bc
Afik Bar <afikbar@gmail.com>
2018-04-17 17:54:05 +0300

    [hw1] add State class (incomplete) :gift:

--- hw1/src/ex1.py
+++ hw1/src/ex1.py
@@ -2,27 +2,57 @@ import search
 import random
 import math
 
-
 ids = ["111111111", "111111111"]
+
+
+class State(object):
+    def __init__(self, grid):
+        self.pos_dict = {
+            'pacman': [(ix, iy) for ix, row in enumerate(grid) for iy, i in enumerate(row) if i == 66],
+            'pills': [(ix, iy) for ix, row in enumerate(grid) for iy, i in enumerate(row) if
+                      i in [11, 21, 31, 41, 51, 71]],
+            'ghosts': {
+                'blue': [(ix, iy) for ix, row in enumerate(grid) for iy, i in enumerate(row) if i in [20, 21]],
+                'yellow': [(ix, iy) for ix, row in enumerate(grid) for iy, i in enumerate(row) if i in [30, 31]],
+                'green': [(ix, iy) for ix, row in enumerate(grid) for iy, i in enumerate(row) if i in [40, 41]],
+                'red': [(ix, iy) for ix, row in enumerate(grid) for iy, i in enumerate(row) if i in [50, 51]],
+            },
+            'poison': [(ix, iy) for ix, row in enumerate(grid) for iy, i in enumerate(row) if i in [71, 77]],
+        }
+
+        self._pillCnt = len(self.pos_dict['pills'])
+
+    def __eq__(self, other):
+        return isinstance(other, State) and self._pillCnt == other._pillCnt
+
+    def __hash__(self):
+        return hash(self._pillCnt)  # consider adding hash for ghosts cnt\dist?
 
 
 class PacmanProblem(search.Problem):
     """This class implements a spaceship problem"""
+
     def __init__(self, initial):
         """Don't forget to set the goal or implement the goal test
         You should change the initial to your own representation"""
-        search.Problem.__init__(self, initial)
-        
+        # receives the input from main in check.py
+        self._size = (len(initial), len(initial[0]))  # size (rows,cols)
+
+        search.Problem.__init__(self, initial)  # we can change the initial (world as we see it)
+
     def actions(self, state):
         """Return the actions that can be executed in the given
         state. The result would typically be a tuple, but if there are
         many actions, consider yielding them one at a time in an
         iterator, rather than building them all at once."""
+        # define how we do expand in search algo (up,down,left,rigt), for pacman only
 
     def result(self, state, action):
         """Return the state that results from executing the given
         action in the given state. The action must be one of
         self.actions(state)."""
+        # ghosts moves here, as "result" from pacman action (moves towards pacman's new pos)
+        # min(man_dist to pacman from one of the available slots)
 
     def goal_test(self, state):
         """ Given a state, checks if this is the goal state, compares to the created goal state"""
@@ -37,4 +67,3 @@ class PacmanProblem(search.Problem):
 
 def create_pacman_problem(game):
     return PacmanProblem(game)
-
