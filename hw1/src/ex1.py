75127da710ee88ae49d3a073688e9017f877a026
Afik Bar <afikbar@gmail.com>
2018-04-26 00:16:08 +0300

    [hw1] renewed State class and some logic around :beer:

--- hw1/src/ex1.py
+++ hw1/src/ex1.py
@@ -7,72 +7,114 @@ from copy import deepcopy
 
 ids = ["111111111", "111111111"]
 
+WALL, PACMAN, CELL = 99, 66, 10
+PILLS, POISON = [11, 21, 31, 41, 51, 71], [71, 77]
+B_GHOST, Y_GHOST, G_GHOST, R_GHOST = [20, 21], [30, 31], [40, 41], [50, 51]
+EATEN_BY = 88
+
+DIRECTIONS = {'R': (0, 1), 'D': (1, 0), 'L': (0, -1), 'U': (- 1, 0)}  # order matters
+
 
 class State(object):
-    wall, pacman, cell = 99, 66, 10
-    pills, poison = [11, 21, 31, 41, 51, 71], [71, 77]
-    bGhost, yGhost, gGhost, rGhost = [20, 21], [30, 31], [40, 41], [50, 51]
-    eatenBy = 88
+    # TODO: rethink state object (remove static vars, getters\setters for pos, etc)
+    # TODO: design as Predicats? atoms of True.
+    last_state = 0  # static
 
-    def __init__(self, grid=None, state=None):
-        if state:
-            self.gridDict = deepcopy(state.gridDict)
-            self.pos_dict = deepcopy(state.pos_dict)
-            self._pillCnt = deepcopy(state._pillCnt)
-        elif grid:
-            self.gridDict = {(x, y): ele for x, row in enumerate(grid) for y, ele in enumerate(row)}
-            # lookup = {66: 'pacman', 11: 'pills', 21: 'pills', 31: 'pills', 41: 'pills', 51: 'pills', 71: 'pills'}
-            # posDtst = {}
-            # for key, value in sorted(d.items()):
-            #     posDtst.setdefault(value, []).append(key)
-            self.pos_dict = {
-                'pacman': [(x, y) for x, row in enumerate(grid) for y, ele in enumerate(row) if ele == self.pacman],
-                'pills': [(x, y) for x, row in enumerate(grid) for y, ele in enumerate(row) if ele in self.pills],
-                'ghosts': {
-                    'red': [(x, y) for x, row in enumerate(grid) for y, ele in enumerate(row) if ele in self.rGhost],
-                    'blue': [(x, y) for x, row in enumerate(grid) for y, ele in enumerate(row) if ele in self.bGhost],
-                    'yellow': [(x, y) for x, row in enumerate(grid) for y, ele in enumerate(row) if ele in self.yGhost],
-                    'green': [(x, y) for x, row in enumerate(grid) for y, ele in enumerate(row) if ele in self.gGhost]
-                },
-                'poison': [(x, y) for x, row in enumerate(grid) for y, ele in enumerate(row) if ele in self.poison],
-                'walls': [(x, y) for x, row in enumerate(grid) for y, ele in enumerate(row) if ele == self.wall]
-            }
-
-            self._pillCnt = len(self.pos_dict['pills'])
+    def __init__(self, grid):
+        State.last_state = State.last_state + 1
+        self._state_num = State.last_state
+        self._gridDict = {(x, y): ele for x, row in enumerate(grid) for y, ele in enumerate(row)}
+        self._pacman = next((cord for cord, ele in self._gridDict.items() if ele == PACMAN), None)
+        self._pills = [cord for cord, ele in self._gridDict.items() if ele in PILLS]
+        self._poison = [cord for cord, ele in self._gridDict.items() if ele in POISON]
+        self._walls = [cord for cord, ele in self._gridDict.items() if ele == WALL]
+        self._ghosts = {
+            R_GHOST[0]: next((cord for cord, ele in self._gridDict.items() if ele in R_GHOST), None),
+            B_GHOST[0]: next((cord for cord, ele in self._gridDict.items() if ele in B_GHOST), None),
+            Y_GHOST[0]: next((cord for cord, ele in self._gridDict.items() if ele in Y_GHOST), None),
+            G_GHOST[0]: next((cord for cord, ele in self._gridDict.items() if ele in G_GHOST), None)
+        }
+        self._ghosts = {k: v for k, v in self._ghosts.items() if v}  # removes keys with empty values
+        self._pill_count = len(self._pills)
 
     @property
-    def pillCnt(self):
-        return self._pillCnt
+    def grid(self):
+        return self._gridDict
 
-    @pillCnt.setter
-    def pillCnt(self, val):
-        self._pillCnt=val
+    @grid.setter
+    def grid(self, dict):
+        self._gridDict = dict
+
+    @property
+    def pacman(self):
+        return self._pacman
+
+    @pacman.setter
+    def pacman(self, cord):
+        self._pacman = cord
+
+    @property
+    def pills(self):
+        return self._pills
+
+    @pills.setter
+    def pills(self, list):
+        self._pills = list
+
+    @property
+    def poison(self):
+        return self._poison
+
+    @poison.setter
+    def poison(self, list):
+        self._poison = list
+
+    @property
+    def walls(self):
+        return self._walls
+
+    @walls.setter
+    def walls(self, walls_list):
+        self._walls = walls_list
+
+    @property
+    def ghosts(self):
+        return self._ghosts
+
+    @ghosts.setter
+    def ghosts(self, ghosts_dict):
+        self._ghosts = ghosts_dict
+
+    @property
+    def pill_count(self):
+        return self._pill_count
+
+    @pill_count.setter
+    def pill_count(self, val):
+        self._pill_count = val
 
     def __eq__(self, other):
-        return isinstance(other, State) and self.gridDict == other.gridDict
+        return isinstance(other, State) and self._gridDict.__eq__(other.grid)
 
     def __lt__(self, other):
-        return hashabledict(self.gridDict).__lt__(hashabledict(other.gridDict))
-
-    def __gt__(self, other):
-        return hashabledict(self.gridDict).__gt__(hashabledict(other.gridDict))
-
-    def __le__(self, other):
-        return hashabledict(self.gridDict).__le__(hashabledict(other.gridDict))
-
-    def __ge__(self, other):
-        return hashabledict(self.gridDict).__ge__(hashabledict(other.gridDict))
+        return isinstance(other, State) and self._pill_count.__lt__(other.pill_count)
 
     def __hash__(self):
-        return hash(hashabledict(self.gridDict))  # consider adding hash for ghosts cnt\dist?
+        return hash(hashabledict(self._gridDict))  # consider adding hash for ghosts cnt\dist?
 
-    def __iter__(self):
-        return iter(self.gridDict)
+    def print(self):
+        print(" State {}:\n".format(self._state_num))
+        row = 0
+        for cord, ele in self._gridDict.items():
+            if row != cord[0]:
+                print("\n")
+                row = row + 1
+            print(ele, ", ", end='')
+        print("\n")
 
 
 class PacmanProblem(search.Problem):
     """This class implements a spaceship problem"""
-    directions = {'R': (0, 1), 'D': (1, 0), 'L': (0, -1), 'U': (- 1, 0)}  # order matters
 
     def __init__(self, initial):
         """Don't forget to set the goal or implement the goal test
@@ -89,13 +131,12 @@ class PacmanProblem(search.Problem):
         many actions, consider yielding them one at a time in an
         iterator, rather than building them all at once."""
         # define how we do expand in search algo (up,down,left,rigt), for pacman only
-        posDict = state.pos_dict
-        pCords = pRow, pCol = posDict['pacman'][0]  # returns a list.. (for coherentic method)
-        # moves = {'U': (pRow - 1, pCol), 'R': (pRow, pCol + 1), 'D': (pRow + 1, pCol), 'L': (pRow - 1, pCol)}
-        # moves = {key:vector_add(pCords,val) for key,val in self.directions.items()}
-        for action, dir in self.directions.items():
-            cord = vector_add(pCords, dir)
-            if (cord not in list(posDict['ghosts'].values())) and (cord not in posDict['poison']):
+        p_cords = state.pacman
+        if state.grid[p_cords] == EATEN_BY:
+            return
+        for action, step in DIRECTIONS.items():
+            cord = vector_add(p_cords, step)  # adds tuples element-wise
+            if (cord not in list(state.ghosts.values())) and (cord not in POISON):
                 yield action
 
     def result(self, state, action):
@@ -104,60 +145,63 @@ class PacmanProblem(search.Problem):
         self.actions(state)."""
         # ghosts moves here, as "result" from pacman action (moves towards pacman's new pos)
         # min(man_dist to pacman from one of the available slots)
-        rslt = State(None, state)
-        posDict = rslt.pos_dict
-        # try:
-        pCords = posDict['pacman'][0]
-        # except Exception as e:
-        #     print("error",e)
-        rslt_pCords = vector_add(pCords, self.directions[action])
-        rslt_pCords = pacmanX, pacmanY = pCords if rslt.gridDict[rslt_pCords] == State.wall else rslt_pCords
-        # move pacman
-        if rslt.gridDict[rslt_pCords] == State.pills[0]:
-            rslt.pillCnt = rslt.pillCnt-1
-            rslt.pos_dict['pills'].remove(rslt_pCords)
-        rslt.gridDict[pCords], rslt.gridDict[rslt_pCords] = State.cell, State.pacman  # old = cell, new = pacman
-        posDict['pacman'][0] = rslt_pCords  # updates posDict
+        rslt = deepcopy(state)
+        p_cords = state.pacman
+        if rslt.grid[p_cords] == EATEN_BY:  # if pacman was eaten dont play
+            return rslt
+        rslt_p_cords = vector_add(p_cords, DIRECTIONS[action])
+        rslt_p_cords = p_cords if rslt.grid[rslt_p_cords] == WALL else rslt_p_cords
+        # move pacman:
+        if rslt.grid[rslt_p_cords] == PILLS[0]:
+            rslt.pill_count -= 1
+            rslt.pills.remove(rslt_p_cords)
+        rslt.grid[p_cords], rslt.grid[rslt_p_cords] = CELL, PACMAN  # old = cell, new = pacman
+        rslt.pacman = rslt_p_cords  # updates position
 
-        for ghost, cord in {k: v for k, v in posDict['ghosts'].items() if v}.items():  # order by ghost order
-            # checks if cord empty
-            gCords = pRow, pCol = cord[0]
-            # moves = {'U': (pRow - 1, pCol), 'R': (pRow, pCol + 1), 'D': (pRow + 1, pCol), 'L': (pRow - 1, pCol)}
-            moves = {key: vector_add(gCords, val) for key, val in self.directions.items() if
-                     rslt.gridDict[vector_add(gCords, val)] not in [State.wall] + list(posDict['ghosts'].values())}
+        # move ghosts:
+        for ghost, g_cords in rslt.ghosts.items():  # order by ghost order
+            # checks if ghost empty
+            moves = {key: vector_add(g_cords, val) for key, val in DIRECTIONS.items() if
+                     rslt.grid[vector_add(g_cords, val)] not in [WALL] + list(rslt.ghosts.values())}
             # filters movement to walls.
-            # shortestKey = min(moves, key=lambda k: abs(moves[k][0] - pacmanX) + abs(moves[k][1] - pacmanY))
-            shortest = min(list(moves.values()), key=lambda v: abs(v[0] - pacmanX) + abs(v[1] - pacmanY))
-            # what happens if same minimum? gets first min, since order is by directions, shoud be ok
+            # get "shortest" move:
+            try:
+                shortest = min(list(moves.values()),
+                               key=lambda v: abs(v[0] - rslt.pacman[0]) + abs(v[1] - rslt.pacman[1]))
+                # what happens if same minimum? gets first min, since order is by DIRECTIONS, shoud be ok
+            except ValueError:
+                continue
             # if shortest is null means ghost stuck
-            if (rslt.gridDict[shortest] == State.poison[0]):  # with pill
-                rslt.gridDict[shortest] = State.pills[0]
-                del posDict['ghosts'][ghost]
-                rslt.pos_dict['poison'].remove(shortest)
+
+            if rslt.grid[shortest] == POISON[0]:  # poison + pill
+                rslt.grid[shortest] = PILLS[0]
+                del rslt.ghosts[ghost]
+                rslt.poison.remove(shortest)
                 # delete ghost and keep pill
 
-            elif rslt.gridDict[shortest] == State.poison[1]:  # no pill
-                rslt.gridDict[shortest] = State.cell
-                del posDict['ghosts'][ghost]
-                rslt.pos_dict['poison'].remove(shortest)
+            elif rslt.grid[shortest] == POISON[1]:  # poison only
+                rslt.grid[shortest] = CELL
+                del rslt.ghosts[ghost]
+                rslt.poison.remove(shortest)
 
-            elif rslt.gridDict[shortest] == State.pacman:  # eats pacman
-                rslt.gridDict[shortest] = State.eatenBy
+            elif rslt.grid[shortest] == PACMAN:  # eats pacman
+                rslt.grid[shortest] = EATEN_BY
+                rslt.ghosts[ghost] = shortest
                 # TODO:what else??
-            else:  # empty cell maybe with pill
-                rslt.gridDict[shortest] = (rslt.gridDict[shortest] % 10) + (
-                        rslt.gridDict[gCords] - rslt.gridDict[gCords] % 10)  # changes empty cell to ghost with no pill
-                posDict['ghosts'][ghost][0] = shortest  # updates posDict
 
-            rslt.gridDict[gCords] = rslt.gridDict[gCords] - (rslt.gridDict[gCords] // 10) * 10 + 10
+            else:  # empty cell maybe with pill
+                rslt.grid[shortest] = (rslt.grid[shortest] % 10) + ghost  # ghost is x0 ghost id
+                # changes destination cell to be with ghost (with or without pill)
+                rslt.ghosts[ghost] = shortest  # updates new pos
+
+            rslt.grid[g_cords] = rslt.grid[g_cords] - ghost + 10
             # old cords gets update
             # old = pill\empty, new = ghost
-
             return rslt
 
     def goal_test(self, state):
         """ Given a state, checks if this is the goal state, compares to the created goal state"""
-        return state.pillCnt == 0
+        return state.pill_count == 0
 
     def h(self, node):
         """ This is the heuristic. It gets a node (not a state,
@@ -166,9 +210,9 @@ class PacmanProblem(search.Problem):
         # TODO: prefer board where ghosts are near poison+pill?
         # TODO: prefer eat pills in direction of ghosts and then runaway?
         # TODO: remove "bad effect" to gain best estimate (i.e. duplicate positions without deleteing)
-        if node.state.gridDict[node.state.pos_dict['pacman'][0]] == State.eatenBy:
+        if node.state.grid[node.state.pacman] == EATEN_BY:
             return sys.maxsize
-        return node.state.pillCnt
+        return node.state.pill_count
 
     """Feel free to add your own functions"""
 
